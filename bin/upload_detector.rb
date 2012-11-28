06ddfc934acaa642e89d4c998ed57d72b10985d8
Franco Sebregondi <me@usershell.org>
2012-11-28 13:03:47 -1000

    make script environment aware

--- bin/upload_detector.rb
+++ bin/upload_detector.rb
@@ -1,17 +1,18 @@
 #!/usr/bin/env ruby
 require "rubygems"
 require "bundler"
-Bundler.setup(:default)
+env = ENV['UPLOAD_DETECTOR_ENV'] || 'development'
+Bundler.setup(:default, env)
 require 'trollop'
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'upload_detector'
 
 opts   = CommandlineOptions.new
-config = AppConfig.new filename: 'upload_detector.yml', initial_data: opts.opts
+config = AppConfig.new filename: 'upload_detector.yml', env: env, initial_data: opts.opts
 
 
-log_file = File.open log_file_path
+log_file = File.open config.log_file
 detector = Detector.new log_file: log_file
 #detector.add_listener AnnotationListener.new if opts[:annotate]
 #detector.add_listener UploadListener.new unless opts[:annotate] || opts[:debug]

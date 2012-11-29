93e0a48562ff227d8b4c5e06b38b93c8fa5ba748
Franco Sebregondi <me@usershell.org>
2012-11-28 22:33:25 -1000

    simpler shell implementation

--- lib/upload_detector/shell.rb
+++ lib/upload_detector/shell.rb
@@ -5,11 +5,12 @@ CommandResult = Struct.new :status, :output
 class Shell
   def execute command, flags=[], input=nil
     result = CommandResult.new
-    IO.popen([command, *flags], 'w+', err: [:child, :out]) do |io|
-      io.write(input) if input
-      io.close_write
-      result.output = io.read
-    end
+    #IO.popen([command, *flags], 'w+', err: [:child, :out]) do |io|
+      #io.write(input) if input
+      #io.close_write
+      #result.output = io.read
+    #end
+    result.output = `#{command} #{flags.join(' ')}`
     result.status = $?
     result
   end

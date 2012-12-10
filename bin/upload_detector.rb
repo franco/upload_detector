961495a57223d339ec23b2cd8d1a044c1bdc34d0
Franco Sebregondi <me@usershell.org>
2012-12-10 12:35:10 -1000

    prioritize logging

--- bin/upload_detector.rb
+++ bin/upload_detector.rb
@@ -19,15 +19,15 @@ detector = Detector.new input_file: input_file
 #detector.add_listener UploadListener.new unless opts[:annotate] || opts[:debug]
 detector.add_listener DebugUploadListener.new if opts[:debug]
 
-import_config = AppConfig.new filename: 'import_trigger.yml', env: env
-detector.add_listener ImportTrigger.new(import_config)
-
 if config.logfile
   #log_level = opts[:debug] ? Log4r::DEBUG : Log4r::INFO
   log_path = File.expand_path(config.logfile, UploadDetector.root)
   logger = Logger.new(:filename => log_path)
   detector.add_listener logger
 end
+
+import_config = AppConfig.new filename: 'import_trigger.yml', env: env
+detector.add_listener ImportTrigger.new(import_config)
 
 forever = config.deamonized
 UploadDetector.new( :detector => detector, :logger => nil ).run(forever)

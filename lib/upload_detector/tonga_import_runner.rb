e480e5fea0ccc663aa29a22c0953a95f066137a8
Franco Sebregondi <me@usershell.org>
2014-01-08 20:14:49 -1000

    fix tonga import runner

--- lib/upload_detector/tonga_import_runner.rb
+++ lib/upload_detector/tonga_import_runner.rb
@@ -4,7 +4,7 @@ class TongaImportRunner < ImportRunner
   protected
 
     def execute_script import
-      identifier = import.upload_reference.account
+      identifier = import.account
       shell.execute run_script, [identifier, import.files].flatten
     end
 
